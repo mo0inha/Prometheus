204a7c76b63d909df99b41e7cfcceb340511c1ad
Mo0inha <arthur.moura.augusto@gmail.com>
2025-02-20 12:22:37 -0300

    fix: dependency injection code reduction

--- Prometheus/Services.Api/Shared/DependencyInjection/DependencyInjection.cs
+++ Prometheus/Services.Api/Shared/DependencyInjection/DependencyInjection.cs
@@ -3,80 +3,45 @@ using Application.Shared.Interfaces;
 using FluentValidation;
 using MediatR;
 
-namespace Services.Api.DependencyInjection
+namespace Services.Api.DependencyInjection;
+
+public static class DependencyInjection
 {
-    public static class DependencyInjection
+    public static IServiceCollection AddApplication(this IServiceCollection services) => services.AddValidators().AddMediatRConfiguration().AddInfrastructure().AddHandlers();
+
+    private static IServiceCollection AddValidators(this IServiceCollection services)
     {
-        public static IServiceCollection AddApplication(this IServiceCollection services)
+        services.AddValidatorsFromAssembly(AssemblyReference.Assembly, includeInternalTypes: true);
+        services.AddSingleton<IValidationProvider, OptionalValidationProvider>();
+        return services;
+    }
+
+    private static IServiceCollection AddMediatRConfiguration(this IServiceCollection services) => services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(AssemblyReference.Assembly));
+
+    private static IServiceCollection AddHandlers(this IServiceCollection services)
+    {
+        var assemblies = AppDomain.CurrentDomain.GetAssemblies();
+
+        foreach (var assembly in assemblies)
         {
-            return services.AddValidators().AddMediatRConfiguration().AddInfrastructure().AddHandlers();
-        }
+            var handlerTypes = assembly.GetTypes().Where(t => t.IsClass && !t.IsAbstract && t.BaseType is { IsGenericType: true } baseType && (baseType.GetGenericTypeDefinition() == typeof(BaseCommand<,,>) || baseType.GetGenericTypeDefinition() == typeof(BaseQuery<,,>))).ToList();
 
-        private static IServiceCollection AddValidators(this IServiceCollection services)
-        {
-            services.AddValidatorsFromAssembly(AssemblyReference.Assembly, includeInternalTypes: true);
-
-            services.AddSingleton<IValidationProvider>(sp => new OptionalValidationProvider(sp));
-
-            return services;
-        }
-
-        private static IServiceCollection AddMediatRConfiguration(
-            this IServiceCollection services
-        )
-        {
-            services.AddMediatR(cfg =>
+            foreach (var handlerType in handlerTypes)
             {
-                cfg.RegisterServicesFromAssembly(AssemblyReference.Assembly);
-            });
-
-            return services;
-        }
-
-        private static IServiceCollection AddHandlers(this IServiceCollection services)
-        {
-            var assemblies = AppDomain.CurrentDomain.GetAssemblies();
-
-            foreach (var assembly in assemblies)
-            {
-                var handlerTypes = assembly.GetTypes().Where(t => t.IsClass && !t.IsAbstract && t.BaseType != null && t.BaseType.IsGenericType && (t.BaseType.GetGenericTypeDefinition() == typeof(BaseCommand<,,>) || t.BaseType.GetGenericTypeDefinition() == typeof(BaseQuery<,,>))).ToList();
-
-                foreach (var handlerType in handlerTypes)
+                foreach (var @interface in handlerType.GetInterfaces().Where(i => i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IRequestHandler<,>)))
                 {
-                    var interfaces = handlerType.GetInterfaces();
-                    foreach (var @interface in interfaces)
-                    {
-                        if (@interface.IsGenericType && @interface.GetGenericTypeDefinition() == typeof(IRequestHandler<,>))
-                        {
-                            Console.WriteLine($"Registering {@interface.FullName} as {handlerType.FullName}");
-                            services.AddScoped(@interface, handlerType);
-                        }
-                    }
+                    Console.WriteLine($"Registering {@interface.FullName} as {handlerType.FullName}");
+                    services.AddScoped(@interface, handlerType);
                 }
             }
-
-            return services;
         }
-
-        private static IServiceCollection AddInfrastructure(this IServiceCollection services)
-        {
-            services.AddScoped<IRepository, Repository>();
-            return services;
-        }
+        return services;
     }
 
-    public class OptionalValidationProvider : IValidationProvider
-    {
-        private readonly IServiceProvider _serviceProvider;
+    private static IServiceCollection AddInfrastructure(this IServiceCollection services) => services.AddScoped<IRepository, Repository>();
+}
 
-        public OptionalValidationProvider(IServiceProvider serviceProvider)
-        {
-            _serviceProvider = serviceProvider;
-        }
-
-        public IValidator<T> GetValidator<T>()
-        {
-            return _serviceProvider.GetService<IValidator<T>>();
-        }
-    }
+public class OptionalValidationProvider(IServiceProvider serviceProvider) : IValidationProvider
+{
+    public IValidator<T>? GetValidator<T>() => serviceProvider.GetService<IValidator<T>>();
 }
